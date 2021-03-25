0ad796cd59b04bd8380b5746ae87f5de14ca3950
steftones <stefansokolowski99@gmail.com>
2021-03-25 21:08:20 +0000

    navbar bug fix

--- client/components/common/Navigation.js
+++ client/components/common/Navigation.js
@@ -19,11 +19,11 @@ const Navigation = ()=>{
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="mr-auto">
         <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/music">Make Music</Nav.Link>
+        <Nav.Link href="/songpage">The Song Community</Nav.Link>
         {!isLoggedIn && <>
           <Nav.Link href="/login">Login</Nav.Link>
           <Nav.Link href="/register">Register</Nav.Link>
-          <Nav.Link href="/music">Make Music</Nav.Link>
-          <Nav.Link href="/songpage">The Song Community</Nav.Link>
         </>}
         {isLoggedIn && <>
           <Nav.Link href="/usersongpage">My Songs</Nav.Link>
