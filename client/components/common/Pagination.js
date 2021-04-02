e358cb503e49d3285cfe832b6c6efe12f679b245
steftones <stefansokolowski99@gmail.com>
2021-04-02 13:56:28 +0100

    sudden heroku could not be served problem

--- client/components/common/Pagination.js
+++ client/components/common/Pagination.js
@@ -3,8 +3,7 @@ import _ from 'lodash'
 
 const Pagination = ({ itemsCount, pageSize, handlePageChange, currentPage }) => {
 
-  const pagesCount = Math.ceil(itemsCount / pageSize) // to prevent only 1 page in pagination
-  // use lodash to create [1 ... pagesCount].map()
+  const pagesCount = Math.ceil(itemsCount / pageSize)
   if (pagesCount === 1) return null
   const pages = _.range(1, pagesCount + 1)
 
